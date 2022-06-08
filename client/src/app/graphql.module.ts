e05b1039f7839157fa994a74c491d577910dc719
tverdokhlibt <tverdokhlibt@gmail.com>
2022-06-08 11:38:00 +0300

    Update

--- client/src/app/graphql.module.ts
+++ client/src/app/graphql.module.ts
@@ -3,10 +3,7 @@ import { ApolloModule, APOLLO_OPTIONS, Apollo } from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-// console.log('isDev', isDevMode());
-// const uri = isDevMode() ? 'http://localhost:5000/graphql' : ''; //TODO: Add Live url of you app backend
-// const uri = isDevMode() ? 'http://localhost:5000/graphql' : 'http://minmekaniker.herokuapp.com:5000/graphql'; //TODO: Add Live url of you app backend
-  const uri = 'http://18.197.140.199:5000/graphql';
+const uri = 'http://18.197.140.199:5000/graphql';
 export function createApollo(httpLink: HttpLink) {
   return {
     link: httpLink.create({ uri }),

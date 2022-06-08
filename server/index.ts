3ad6796e9749d349b5c4a4c79e48a5ada486889a
tverdokhlibt <tverdokhlibt@gmail.com>
2022-06-08 12:36:39 +0300

    Update

--- server/index.ts
+++ server/index.ts
@@ -12,56 +12,8 @@ const app = express();
 app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  // app.use(enforce.HTTPS({ trustProtoHeader: true }));
 }
-
-// const config = {
-// // Reads from pm2.config.js ENVs - or /script/env_dev.sh
-//   user: process.env.SQL_USER,
-//   password: process.env.SQL_PASSWORD,
-//   database: process.env.SQL_DATABASE,
-//   host: process.env.SQL_HOST,
-//   port: +process.env.SQL_PORT,
-//   firebaseProject: process.env.FIREBASE_PROJECT,
-// };
-// console.log("Running as user: " + config.user);
-// console.log("Running as pwd: " + config.password.substring(0, 4) + "*****");
-// console.log("Running on db: " + config.database);
-// console.log("Running on db: " + config.database);
-
-// const opts = {
-//   subscriptions: true,
-//   watchPg: true,
-//   dynamicJson: true,
-//   setofFunctionsContainNulls: false,
-//   ignoreRBAC: false,
-//   ignoreIndexes: false,
-//   showErrorStack: true,
-
-//   extendedErrors: ["hint", "detail", "errcode"],
-//   // skipPlugins: [NodePlugin],
-//   skipPlugins: [],
-//   appendPlugins: [
-//     require("@graphile-contrib/pg-simplify-inflector"),
-//     ConnectionFilterPlugin,
-//     // postgraphile_tags,
-//   ],
-//   exportJsonSchemaPath: "schema.json",
-//   exportGqlSchemaPath: "schema.graphql",
-//   graphiql: true,
-//   enhanceGraphiql: true,
-//   enableQueryBatching: true,
-// };
-// console.log(opts);
-// app.use(postgraphile(config, "app_public", opts));
-// app.use(
-//   postgraphile(config, "app_public", {
-// appendPlugins: [ConnectionFilterPlugin],
-//     watchPg: true,
-//     graphiql: true,
-//     enhanceGraphiql: true,
-//   })
-// );
 
 const db_url = 
   process.env.NODE_ENV === "production" ? 
